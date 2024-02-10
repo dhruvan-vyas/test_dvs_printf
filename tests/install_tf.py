39d73b6bdf11493ed94ee9f78102428572203fe7
dhruvan_vyas <98950841+dhruvan-vyas@users.noreply.github.com>
2024-02-11 04:22:58 +0530

    tests

--- tests/install_tf.py
+++ tests/install_tf.py
@@ -1,2 +1,3 @@
-from platform import python_version
-print(python_version())
+# from platform import python_version
+# import os
+# if "3.12" not in str(python_version()):os.system('pip3 install tensorflow --no-deps')
